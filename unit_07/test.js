12e27e0c63027b19640dd55661599b5dad15cde5
Fadoua <fades.devs@gmail.com>
2025-03-12 01:22:19 +0000

    completed JS activity 2

--- unit_07/test.js
+++ unit_07/test.js
@@ -4,6 +4,7 @@ const bill = document.getElementById('bill');
 const per1 = document.getElementById('per1');
 const per2 = document.getElementById('per2');
 const per3 = document.getElementById('per3');
+const largest = document.getElementById('largest')
 
 // per1.addEventListener('click', getTip);
 
@@ -17,16 +18,49 @@ const per3 = document.getElementById('per3');
 bill.addEventListener('input', triggerTip);
 
 function triggerTip() {
-    per1.addEventListener('click', getValues)
+
+    per1.addEventListener('click', getValues1)
+        function getValues1() {
+            let billValue = bill.value
+            let tipPerc = per1.innerHTML.slice(0, 1)
+            console.log(tipPerc)
+            document.getElementById('final').innerHTML = billValue * (tipPerc / 100)
+            return
+        }
+
+    per2.addEventListener('click', getValues2)
+        function getValues2() {
+            let billValue = bill.value
+            let tipPerc = per2.innerHTML.slice(0, 2)
+            console.log(tipPerc)
+            document.getElementById('final').innerHTML = billValue * (tipPerc / 100)
+            return
+        }
+    
+        per3.addEventListener('click', getValues3)
+        function getValues3() {
+            let billValue = bill.value
+            let tipPerc = per3.innerHTML.slice(0, 2)
+            console.log(tipPerc)
+            document.getElementById('final').innerHTML = billValue * (tipPerc / 100)
+            return
+        }
+
+        largest.addEventListener('click', getValues4)
+        function getValues4() {
+            let billValue = bill.value
+            let tipMid = per2.innerHTML.slice(0, 2)
+            let tipBig = per3.innerHTML.slice(0, 2)
+            let maxValue = Math.max((billValue * (tipBig / 100)), ((billValue * (tipMid / 100)) + 2))
+            document.getElementById('final').innerHTML = maxValue
+            return
+        }
+    
 }
 
-function getValues() {
-    let billValue = bill.value
-    let tipPerc = per1.innerHTML.slice(0, 1)
-    console.log(billValue * (tipPerc / 100))
-    document.getElementById('final').innerHTML = billValue * (tipPerc / 100)
-    return
-}
+
+
+
 
 // var x = bill.addEventListener('input', getvalue);
 // console.log(x);
